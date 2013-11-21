0978f927b264a682079d71941235e5da482f91b4
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2013-11-21 23:33:32 +0800

    Adds Designs to Form

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,9 @@
 module WelcomeHelper
+  def landing?
+    request.fullpath == "/"
+  end
+
+  def path?(url)
+    request.fullpath.include? url
+  end
 end
--- spec/helpers/welcome_helper_spec.rb
+++ spec/helpers/welcome_helper_spec.rb
@@ -1,3 +1,11 @@
 require 'spec_helper'
 describe WelcomeHelper do
+  describe "#landing?" do
+    it "returns true if current path is root"
+    it "returns false if current path is not root"
+  end
+
+  describe "#path" do
+    it "returns true if param is part of the url"
+  end
 end
